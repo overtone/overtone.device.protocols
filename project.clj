2d03f8d28c31d7e6b6934b2b2e1a124f481aad83
Sam Aaron <samaaron@gmail.com>
2012-04-06 20:54:02 +0100

    bump to 0.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject overtone/device.protocols "0.0.2"
+(defproject overtone/device.protocols "0.0.3"
   :description "Abstraction for external controllers such as grids, sliders, pots and LEDs."
   :url "https://github.com/overtone/overtone.device.protocols"
   :dependencies [[org.clojure/clojure "1.3.0"]])
